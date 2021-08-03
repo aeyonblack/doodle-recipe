e2f7f5f6c60e757d4d7ddfff89f2c8fea2695c52
tanya masvita <tanyamasvitaog@gmail.com>
2021-08-03 18:53:04 +0200

    Initial commit

---
+++ app/src/main/java/com/tanya/doodlerecipe/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.tanya.doodlerecipe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Hello Compose")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = {}) {
+                    Text(text = "Click Me")
+                }
+            }
+        }
+    }
+}
+
