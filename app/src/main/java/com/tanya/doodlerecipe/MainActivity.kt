fe7535cbb041c6301b86732ab7019170861204e7
tanya masvita <tanyamasvitaog@gmail.com>
2021-08-04 20:37:07 +0200

    Trying out rows

--- app/src/main/java/com/tanya/doodlerecipe/MainActivity.kt
+++ app/src/main/java/com/tanya/doodlerecipe/MainActivity.kt
@@ -1,47 +1,107 @@
 package com.tanya.doodlerecipe
 
-import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-
+import com.tanya.doodlerecipe.ui.theme.DoodleRecipeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column {
-                Image(painter = painterResource(id = R.drawable.happy_meal_small),
-                    contentDescription = "happy meal",
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop)
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "Happy Meal",
-                         style = TextStyle(
-                             fontSize = 26.sp
-                         )
-                    )
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "800 Calories")
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "Burger and Fries")
-                }
+            DoodleRecipeTheme {
+                Conversation(messages = SampleData.conversationSample)
             }
         }
+    }
+}
+
+data class Message(val author:String, val body:String)
+
+@Composable
+fun MessageCard(msg:Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.profileimg),
+            contentDescription = "Contact profile image",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape)
+        )
+
+        // Horizontal spacer between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // Keep track if message is expanded or not
+        var isExpanded by remember { mutableStateOf(false)}
+
+        // Surface color will be updates gradually from one color to the other
+        val surfaceColor: Color by animateColorAsState(
+            if (isExpanded) MaterialTheme.colors.primary else MaterialTheme.colors.surface
+        )
+
+        // Toggle the expanded variable when column is clicked
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            // Vertical spacer between author name and message
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                elevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.body2,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) {
+            message -> MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    DoodleRecipeTheme {
+        Conversation(messages = SampleData.conversationSample)
     }
 }
 
